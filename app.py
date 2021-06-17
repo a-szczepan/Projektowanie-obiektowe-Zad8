d740afba8637f9f6cb10e1d87389a42c0afd5e0e
a-szczepan <szczepanczykangelika@gmail.com>
2021-06-17 10:47:30 +0200

    flask app

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'A. Szczepanczyk - projektowanie obiektowe, zadanie 8'
+    return "<span> A. Szczepanczyk - projektowanie obiektowe, zadanie 8 </span>"
